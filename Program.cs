16f32bc6a00057f690d6f342b9a16ae1dde7e387
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-04-12 12:21:19 -0700

    step 3

--- Program.cs
+++ Program.cs
@@ -37,3 +37,36 @@ var ceo = new OpenAIChatAgent(
     """)
     .RegisterMessageConnector()
     .RegisterPrintFormatMessageHook();
+
+// Step 3: Create the CMO
+var kernelBuilder = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion(modelId: gpt3_5, openAIClient: openaiClient);
+var bingSearch = new BingConnector(bingSearchAPIKey);
+var webSearchPlugin = new WebSearchEnginePlugin(bingSearch);
+kernelBuilder.Plugins.AddFromObject(webSearchPlugin);
+var kernel = kernelBuilder.Build();
+var cmo = new SemanticKernelAgent(
+       kernel: kernel,
+       name: "CMO",
+       systemMessage: """
+       You are CMO, you report to Elon and you answer all market-related question.
+
+       To make sure you have the most up-to-date information, you can use the web search plugin to search for information on the web before answering questions.
+       """)
+    .RegisterMessageConnector()
+    .RegisterPrintFormatMessageHook();
+
+// Step 4: Create the workflow
+// define the transition among group members
+// we only allow the following transitions:
+// user -> ceo
+// ceo -> cmo
+// cmo -> ceo
+// ceo -> user
+
+var user2Ceo = Transition.Create(user, ceo);
+var ceo2Cmo = Transition.Create(ceo, cmo);
+var cmo2Ceo = Transition.Create(cmo, ceo);
+var ceo2User = Transition.Create(ceo, user);
+
+var workflow = new Graph([user2Ceo, ceo2Cmo, cmo2Ceo, ceo2User]);
